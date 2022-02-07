a6f386e25f81b2d3bfc778b662cf641ecf18ba05
Julian Barrios <julianbarrios0821@gmail.com>
2022-02-07 10:09:53 -0600

    html layout

---
+++ app.js
@@ -0,0 +1,4 @@
+const pokeCard = document.querySelector('[data-card]');
+const pokeName = document.querySelector('[data-name]');
+const pokeImg = document.querySelector('[data-img]');
+const pokeImg =
